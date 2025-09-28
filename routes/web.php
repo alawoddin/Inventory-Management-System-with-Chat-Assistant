c215af2164060d5e1b2cc14d049701b033f8f8c8
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-28 16:55:23 +0430

    the chat assistant and conversation is done

--- routes/web.php
+++ routes/web.php
@@ -248,6 +248,7 @@ Route::controller(ChatController::class)->group(function() {
     Route::get('/chat-assistants/chat/{assistantId}', 'ChatAssistants')->name('chat-assistants.chat');
     Route::post('/chat-assistants/send/{assistantId}', 'ChatSendMessage')->name('chat-assistants.send');
     Route::get('/chat-assistants/new/{assistantId}', 'StartNewConversation')->name('chat-assistants.new');
+    Route::get('/chat-assistants/{assistantId}/conversation/{conversationId}', 'SelecteConversation')->name('chat-assistants.select');
 
 });
 
